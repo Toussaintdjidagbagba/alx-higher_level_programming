f33ee7476b5a39da960407e75d264c3a60a4718d
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2023-01-03 00:41:16 +0100

    update

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,6 +2,8 @@
 
 /**
  * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
 int check_cycle(listint_t *list)
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,11 +3,7 @@
 
 #include <stdlib.h>
 
-/**
- * struct listint_s - singly linked list
- * @next: points to the next node
- * Description: singly linked list node structure
- */
+
 typedef struct listint_s
 {
 	int n;
