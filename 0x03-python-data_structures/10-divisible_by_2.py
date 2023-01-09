24a1696b357c8eb800181a14549546c9fae67dba
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2023-01-09 10:53:23 +0100

    structure data python

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    n_list = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            n_list.append(True)
+        else:
+            n_list.append(False)
+    return new_list
