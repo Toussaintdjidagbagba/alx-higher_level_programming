cb88b1c9da1f1d8cf83424f315ef807858b58e75
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2023-01-11 23:46:07 +0100

    update and #advanced

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
     i = list(a_dictionary.keys())[0]
     j = a_dictionary[i]
     for p, m in a_dictionary.items():
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
     dicto = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     new_ro = 0
     for i in range(len(roman_string)):
