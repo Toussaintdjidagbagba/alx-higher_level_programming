6f28ec1eb1a008188c257406a7c72013b7395013
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2023-01-23 15:35:33 +0100

    print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             ret += 1
         except IndexError:
-            break
+            print("")
     print("")
     return (ret)
     
