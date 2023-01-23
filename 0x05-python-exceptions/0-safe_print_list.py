cff58dbc3409e7c6b40ce9a421e8907e0b71ddc0
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2023-01-23 15:42:20 +0100

    print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-	count = 0
-	if x == 0:
-		print()
-		return count
-	try:
-		for i in range(x):
-			print("{}".format(my_list[i]), end="")
-			count++
-	except Exception as e:
-		print()
-	print()
-	return count
+	i = 0
+    if x == 0:
+        print()
+        return i
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+        print()
+    except IndexError:
+        print()
+        return i
+    return i + 1
