921beecf44a26128955d129ff5e3a7889c2ee891
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2023-01-24 02:03:13 +0100

    update

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,18 +4,6 @@ import sys
 
 
 def safe_print_integer_err(value):
-    """Prints an integer with "{:d}".format().
-
-    If a ValueError message is caught, a corresponding
-    message is printed to standard error.
-
-    Args:
-        value (int): The integer to print.
-
-    Returns:
-        If a TypeError or ValueError occurs - False.
-        Otherwise - True.
-    """
     try:
         print("{:d}".format(value))
         return (True)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    """Returns the division of a by b."""
     try:
         div = a / b
     except (TypeError, ZeroDivisionError):
