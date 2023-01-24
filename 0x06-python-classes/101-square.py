be68d04aab8e659ad1adc206c1d0e0fa84bc3f3f
Emmanuel DJIDAGBAGBA <emmanueldjidagbagba@gmail.com>
2023-01-25 00:04:55 +0100

    Python class

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""Define a class Square."""
+
 
 class Square:
     def __init__(self, size=0, position=(0, 0)):
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+
+
 class Square:
+    """Represent a square."""
+
     def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
     @property
     def size(self):
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
